4df2f6bbc21d32bc645b9cf426b6ea93633547fb
Charles Garrett <charleslavong@gmail.com>
2024-06-25 15:57:43 -0400

    chore: /wp-content fixes

--- bin/replace_all.sh
+++ bin/replace_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#./bin/search_and_replace.sh public\/blog /wp-content https://pages.near.org/wp-content
-./bin/search_and_replace.sh public\/blog "\/wp\-includes" "https\:\/\/pages\.near\.org\/wp\-includes"
+./bin/search_and_replace.sh public\/blog "\/wp\-content" "https\:\/\/pages\.near\.org\/wp\-content"
 
+#./bin/search_and_replace.sh public\/blog "\/wp\-includes" "https\:\/\/pages\.near\.org\/wp\-includes"
 #./bin/search_and_replace.sh public\/blog "\s\/wp\-content" "\shttps\:\/\/pages\.near\.org\/wp\-content"
 #./bin/search_and_replace.sh public\/blog "\=\"\/wp\-content" "\=\"https\:\/\/pages\.near\.org\/wp-content"
 #./bin/search_and_replace.sh public\/blog "\=\/wp\-content" "\=https\:\/\/pages\.near\.org\/wp\-content"
