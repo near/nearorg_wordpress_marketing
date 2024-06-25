4202615aa3ab47f01953a3f0274b0a012549d833
Charles Garrett <charleslavong@gmail.com>
2024-06-25 16:19:21 -0400

    chore: fixes the removal of the unwanted s3 link

--- bin/replace_all.sh
+++ bin/replace_all.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 ./bin/search_and_replace.sh public\/blog "\/wp\-content" "https\:\/\/pages\.near\.org\/wp\-content"
-
-#./bin/search_and_replace.sh public\/blog "\/wp\-includes" "https\:\/\/pages\.near\.org\/wp\-includes"
-#./bin/search_and_replace.sh public\/blog "\s\/wp\-content" "\shttps\:\/\/pages\.near\.org\/wp\-content"
-#./bin/search_and_replace.sh public\/blog "\=\"\/wp\-content" "\=\"https\:\/\/pages\.near\.org\/wp-content"
-#./bin/search_and_replace.sh public\/blog "\=\/wp\-content" "\=https\:\/\/pages\.near\.org\/wp\-content"
-#./bin/search_and_replace.sh public\/blog "\?paged\=" "page\/"
-#./bin/search_and_replace.sh public\/blog\/near-foundation-announces-policy-principles ' Law Commission’s consultation paper dealing with law reform proposals in the context of digital assets and personal property rights is available <a href=https://s3-eu-west-2.amazonaws.com/lawcom-prod-storage-11jsxou24uy7q/uploads/2022/07/Digital-Assets-Consultation-Paper-Law-Commission-1.pdf>here</a>, and the' ""
+./bin/search_and_replace.sh public\/blog "\/wp\-includes" "https\:\/\/pages\.near\.org\/wp\-includes"
+./bin/search_and_replace.sh public\/blog "\s\/wp\-content" "\shttps\:\/\/pages\.near\.org\/wp\-content"
+./bin/search_and_replace.sh public\/blog "\=\"\/wp\-content" "\=\"https\:\/\/pages\.near\.org\/wp-content"
+./bin/search_and_replace.sh public\/blog "\=\/wp\-content" "\=https\:\/\/pages\.near\.org\/wp\-content"
+./bin/search_and_replace.sh public\/blog "\?paged\=" "page\/"
+./bin/search_and_replace.sh public\/blog\/near-foundation-announces-policy-principles "\=https\:\/\/s3\-eu\-west\-2\.amazonaws\.com\/lawcom\-prod\-storage\-11jsxou24uy7q\/uploads\/2022\/07\/Digital\-Assets\-Consultation\-Paper\-Law\-Commission\-1\.pdf" ""
